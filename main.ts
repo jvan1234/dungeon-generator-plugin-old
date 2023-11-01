f9cf71b3c2012593f651c44f1d19cb1e0b914f1f
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-01 15:20:17 -0400

    Added water and grass :)

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-function Generate_Random_Dungeon (length: number, width: number) {
+function Generate_Random_Dungeon (length: number, width: number, water_chance: number) {
     tiles.setCurrentTilemap(tilemap`GeneratedDungeon`)
     tiles.setTileAt(tiles.getTileLocation(1, 1), assets.tile`myTile`)
     // This code implements walls at the edges of the screen by checking if the x/y index was either 0 or the length/width
@@ -6,11 +6,25 @@ function Generate_Random_Dungeon (length: number, width: number) {
         for (let yIndex = 0; yIndex <= width; yIndex++) {
             if (xIndex == 0 || xIndex == length) {
                 tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
+                tiles.setWallAt(tiles.getTileLocation(xIndex, yIndex), true)
             }
             if (yIndex == 0 || yIndex == width) {
                 tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
+                tiles.setWallAt(tiles.getTileLocation(xIndex, yIndex), true)
+            }
+        }
+    }
+    // This code will place grass/water along the dungeon.
+    for (let xIndex = 0; xIndex <= length; xIndex++) {
+        for (let yIndex = 0; yIndex <= width; yIndex++) {
+            if (!(tiles.tileAtLocationIsWall(tiles.getTileLocation(xIndex, yIndex)) || tiles.tileAtLocationEquals(tiles.getTileLocation(xIndex, yIndex), assets.tile`myTile`))) {
+                if (Math.percentChance(water_chance)) {
+                    tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`water`)
+                } else {
+                    tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`grassA`)
+                }
             }
         }
     }
 }
-Generate_Random_Dungeon(6, 6)
+Generate_Random_Dungeon(6, 6, 10)
