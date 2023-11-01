493f822ddb16f237421c16fae1400031e7dacf95
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-01 15:12:16 -0400

    Sets a starting position for a player

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 function Generate_Random_Dungeon (length: number, width: number) {
     tiles.setCurrentTilemap(tilemap`GeneratedDungeon`)
+    tiles.setTileAt(tiles.getTileLocation(1, 1), assets.tile`myTile`)
+    // This code implements walls at the edges of the screen by checking if the x/y index was either 0 or the length/width
     for (let xIndex = 0; xIndex <= length; xIndex++) {
         for (let yIndex = 0; yIndex <= width; yIndex++) {
-            tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
+            if (xIndex == 0 || xIndex == length) {
+                tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
+            }
+            if (yIndex == 0 || yIndex == width) {
+                tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
+            }
         }
     }
 }
-Generate_Random_Dungeon(4, 4)
+Generate_Random_Dungeon(6, 6)
