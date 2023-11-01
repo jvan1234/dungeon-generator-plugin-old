6e606d0d739924a6b89d7709a7bc485dbe15d8bb
jvan1234 <72505202+jvan1234@users.noreply.github.com>
2023-11-01 15:49:08 -0400

    Downgraded, just to test

--- main.ts
+++ main.ts
@@ -1,63 +1,10 @@
-//% weight=100 color=#CC6600 icon="▓"
-namespace Dungeon_Generation {
-    /**
-     * TODO: This function generates a random dungeon given some parameters
-     * @param length describes how long the dungeon will be, in tiles. eg: 10
-     */
-    //% block
-    export function Generate_Dungeon(length: number, width: number, water_chance: number) {
-        let yIndex: number;
-        let yIndex2: number;
+namespace dungeons {
+    export function Generate_Dungeon (length: number, width: number) {
         tiles.setCurrentTilemap(tilemap`GeneratedDungeon`)
-        tiles.setTileAt(tiles.getTileLocation(1, 1), assets.tile`
-                myTile
-            `)
-        //  This code implements walls at the edges of the screen by checking if the x/y index was either 0 or the length/width
-        let xIndex = 0
-        while (xIndex <= length) {
-            yIndex = 0
-            while (yIndex <= width) {
-                if (xIndex == 0 || xIndex == length) {
-                    tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`
-                            wall
-                        `)
-                    tiles.setWallAt(tiles.getTileLocation(xIndex, yIndex), true)
-                }
-                
-                if (yIndex == 0 || yIndex == width) {
-                    tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`
-                            wall
-                        `)
-                    tiles.setWallAt(tiles.getTileLocation(xIndex, yIndex), true)
-                }
-                
-                yIndex += 1
+        for (let xIndex = 0; xIndex <= length; xIndex++) {
+            for (let yIndex = 0; yIndex <= width; yIndex++) {
+                tiles.setTileAt(tiles.getTileLocation(xIndex, yIndex), assets.tile`wall`)
             }
-            xIndex += 1
-        }
-        //  This code will place grass/water along the dungeon.
-        let xIndex2 = 0
-        while (xIndex2 <= length) {
-            yIndex2 = 0
-            while (yIndex2 <= width) {
-                if (!(tiles.tileAtLocationIsWall(tiles.getTileLocation(xIndex2, yIndex2)) || tiles.tileAtLocationEquals(tiles.getTileLocation(xIndex2, yIndex2), assets.tile`
-                        myTile
-                    `))) {
-                    if (Math.percentChance(water_chance)) {
-                        tiles.setTileAt(tiles.getTileLocation(xIndex2, yIndex2), assets.tile`
-                                water
-                            `)
-                    } else {
-                        tiles.setTileAt(tiles.getTileLocation(xIndex2, yIndex2), assets.tile`
-                                grassA
-                            `)
-                    }
-                    
-                }
-                
-                yIndex2 += 1
-            }
-            xIndex2 += 1
         }
     }
 }
